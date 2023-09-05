b3afceb1f9b1ba36677bc63de21d0d25b18b6b38
shivasai <139239760+jajapuramshivasai@users.noreply.github.com>
2023-09-05 10:55:57 -0700

    Update be.py

--- be.py
+++ be.py
@@ -1,6 +1,14 @@
 import pyautogui
 
-pyautogui.click(1222, 644)
-for i in range(100):
-    pyautogui.write('i love you bebe',0.01)
-    pyautogui.press('enter') 
+####get the required position of curser at wattsapp type bar
+pyautogui.position()
+
+####now comment the line 4
+####put the positions in the pyautogui.click(x,y)
+####uncomment below code and run it
+
+
+# pyautogui.click(1222, 644)
+# for i in range(100):
+#     pyautogui.write('i love you bebe',0.01)
+#     pyautogui.press('enter') 
