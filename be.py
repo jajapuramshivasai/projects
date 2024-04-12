c386d02cdc00cebef81a2d652e59176700503fa4
shivasai <139239760+jajapuramshivasai@users.noreply.github.com>
2024-04-13 00:48:57 +0530

    Update be.py

--- be.py
+++ be.py
@@ -12,5 +12,5 @@ pyautogui.position()
 
 # pyautogui.click(1222, 644)
 # for i in range(100):
-#     pyautogui.write('i love you bebe',0.01)
+#     pyautogui.write('**SPAM**',0.01)
 #     pyautogui.press('enter') 
